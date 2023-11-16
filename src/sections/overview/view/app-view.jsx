fe5f015d00a614f1aab3df515166bfc0604b5e88
thang <hoangducthang27082002@gmail.com>
2023-11-16 14:14:42 +0700

    iott

--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
@@ -12,15 +12,18 @@ import AppWidgetSummary from '../app-widget-summary';
 
 // ----------------------------------------------------------------------
 
-const chatSocket = new WebSocket("wss://iotplan.onrender.com/ws/chat/2/");
+const chatSocket = new WebSocket('wss://iotplan.onrender.com/ws/chat/2/');
 
 export default function AppView() {
   // const [door, setDoor] = useState(true);
   // const [name, setName] = useState('');
   const [nhietdo, setNhietdo] = useState('');
   const [doam, setDoam] = useState('');
+  const [doamdat, setDoamdat] = useState('');
+
   const [arrNhietdo, setArrNhietdo] = useState([]);
   const [arrDoam, setArrDoam] = useState([]);
+  const [arrDoamDat, setArrDoamDat] = useState([]);
 
   const [chat, setChat] = useState([]);
 
@@ -47,48 +50,59 @@ export default function AppView() {
         const dateTimePart = match[2];
 
         const [number, sensor] = numberPart.split(' ');
-    
+
         // Chuyển đổi số từ chuỗi thành float và làm tròn đến số thập phân thứ nhất
         const roundedNumber = parseFloat(number).toFixed(1);
-    
+
         // console.log("thời gian" + tg );
 
-        console.log("Số:", roundedNumber);
-        console.log("Sensor", sensor);
-        console.log("Ngày giờ:", dateTimePart);
-        if(sensor.toString() === "nhietdo"){
+        console.log('Số:', roundedNumber);
+        console.log('Sensor', sensor);
+        console.log('Ngày giờ:', dateTimePart);
+        if (sensor.toString() === 'nhietdo') {
           const newObject = {
-            'title': sensor,
-            'temperature': roundedNumber, 
-            'datetime': dateTimePart
-          }
-          setArrNhietdo(prevArr => [...prevArr, newObject]);
+            title: sensor,
+            temperature: roundedNumber,
+            datetime: dateTimePart,
+          };
+          setArrNhietdo((prevArr) => [...prevArr, newObject]);
           console.log(arrNhietdo);
           setNhietdo(roundedNumber);
-        } else{
+        } else if (sensor.toString() === 'doam') {
           const newObject = {
-            'title': sensor,
-            'humanlity': roundedNumber, 
-            'datetime': dateTimePart
-          }
-          setArrDoam(prevArr => [...prevArr, newObject]);
+            title: sensor,
+            humanlity: roundedNumber,
+            datetime: dateTimePart,
+          };
+          setArrDoam((prevArr) => [...prevArr, newObject]);
           console.log(arrDoam);
           setDoam(roundedNumber);
+        } else {
+          const newObject = {
+            title: sensor,
+            doAmDat: roundedNumber,
+            datetime: dateTimePart,
+          };
+          setArrDoamDat((prevArr) => [...prevArr, newObject]);
+          console.log(arrDoamDat);
+          setDoamdat(roundedNumber);
         }
+      }
     } else {
-        console.log("Không tìm thấy số và chuỗi trong chuỗi.");
-    }
+      console.log('Không tìm thấy số và chuỗi trong chuỗi.');
     }
   };
 
-  const datetimeArray = arrNhietdo.map(item => item.datetime);
-  const nhietdoArray = arrNhietdo.map(item => item.temperature);
+  const datetimeArray = arrNhietdo.map((item) => item.datetime);
+  const nhietdoArray = arrNhietdo.map((item) => item.temperature);
 
+  const datetimeArrayDoam = arrDoam.map((item) => item.datetime);
+  const doamArray = arrDoam.map((item) => item.humanlity);
 
-  const datetimeArrayDoam = arrDoam.map(item => item.datetime);
-  const doamArray = arrDoam.map(item => item.humanlity);
+  // const datetimeArrayDoamdat = arrDoamDat.map((item) => item.datetime);
+  const doamdatArray = arrDoamDat.map((item) => item.doAmDat);
 
-  console.log("datetimeArray: " ,datetimeArray);
+  console.log('datetimeArray: ', datetimeArray);
 
   return (
     <Container maxWidth="xl">
@@ -125,70 +139,75 @@ export default function AppView() {
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
             title="Độ ẩm đất"
-            total={`${90} %`}
+            total={`${doamdat} %`}
             color="error"
             icon={<WaterDropIcon sx={{ fontSize: 60, m: 0.8, color: '#00b8d9' }} />}
           />
         </Grid>
-        <Grid  container xs={12} sm={12} md={12} >
-        <Grid xs={12} sm={6}>
-          <AppWebsiteVisits
-            title="Biểu đồ nhiệt độ"
-            subheader="Cập nhật gần nhất"
-            chart={{
-              labels: datetimeArray,
-              series: [
-                {
-                  name: 'Nhiệt độ',
-                  type: 'area',
-                  fill: 'gradient',
-                  data: nhietdoArray,
-                },
-                // {
-                //   name: 'Team C',
-                //   type: 'line',
-                //   fill: 'solid',
-                //   data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
-                // },
-              ],
-            }}
-          />
-        </Grid>
-        <Grid xs={12} md={12} lg={6} sx={6}>
-          <AppWebsiteVisits
-            title="Biểu đồ độ ẩm không khí"
-            subheader="Cập nhật gần nhất"
-            chart={{
-              labels: datetimeArrayDoam,
-              series: [
-                // {
-                //   name: 'Team A',
-                //   type: 'column',
-                //   fill: 'solid',
-                //   data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-                // },
-                {
-                  name: 'Độ ẩm',
-                  type: 'area',
-                  fill: 'gradient',
-                  data: doamArray,
-                },
-              ],
-              tooltip: {
-                y: {
-                  formatter: (value) => {
-                    if (typeof value !== 'undefined') {
-                      return `${value.toFixed(1)} °C`;
-                    }
-                    return value;
+        <Grid container xs={12} sm={12} md={12}>
+          <Grid xs={12} sm={6}>
+            <AppWebsiteVisits
+              title="Biểu đồ nhiệt độ"
+              subheader="Cập nhật gần nhất"
+              chart={{
+                labels: datetimeArray,
+                series: [
+                  {
+                    name: 'Nhiệt độ',
+                    type: 'area',
+                    fill: 'gradient',
+                    data: nhietdoArray,
+                  },
+                  // {
+                  //   name: 'Team C',
+                  //   type: 'line',
+                  //   fill: 'solid',
+                  //   data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+                  // },
+                ],
+              }}
+            />
+          </Grid>
+          <Grid xs={12} md={12} lg={6} sx={6}>
+            <AppWebsiteVisits
+              title="Biểu đồ độ ẩm không khí"
+              subheader="Cập nhật gần nhất"
+              chart={{
+                labels: datetimeArrayDoam,
+                series: [
+                  // {
+                  //   name: 'Team A',
+                  //   type: 'column',
+                  //   fill: 'solid',
+                  //   data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+                  // },
+                  {
+                    name: 'Độ ẩm',
+                    type: 'area',
+                    fill: 'gradient',
+                    data: doamArray,
+                  },
+                  {
+                    name: 'Độ ẩm đất',
+                    type: 'line',
+                    fill: 'solid',
+                    data: doamdatArray,
+                  },
+                ],
+                tooltip: {
+                  y: {
+                    formatter: (value) => {
+                      if (typeof value !== 'undefined') {
+                        return `${value.toFixed(1)} °C`;
+                      }
+                      return value;
+                    },
                   },
                 },
-              },
-            }}
-          />
+              }}
+            />
+          </Grid>
         </Grid>
-        </Grid>
-        
       </Grid>
     </Container>
   );
