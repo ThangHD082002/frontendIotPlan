bd4c133d74097d0c4bb610ef92d43a60767dd669
thang <hoangducthang27082002@gmail.com>
2023-11-19 16:51:07 +0700

    iot

--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -16,6 +16,7 @@ const chatSocket = new WebSocket('wss://iotplan.onrender.com/ws/chat/2/');
 
 
 
+
 // const chatSocket = new WebSocket('ws://localhost:8000/ws/chat/2/');
 
 export default function AppView() {
